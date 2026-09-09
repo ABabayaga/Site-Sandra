a24893ccc66755c2cae8ab4eff34236f2dd930de
ABabayaga <alef_rdl@hotmail.com>
2026-09-09 12:03:26 -0400

    Alterando mensagem whatsapp

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -9,7 +9,7 @@ export const WhatsAppButton: React.FC = () => {
                     <span className="absolute top-full right-3 h-2.5 w-2.5 -mt-1.5 rotate-45 bg-blue-950 shadow-lg shadow-black/15" />
                 </span>
                 <a
-                    href="https://wa.me/5567981473191?text=Ol%C3%A1!%20Gostaria%20de%20agendar%20uma%20visita."
+                    href="https://wa.me/5567981473191?text=Ol%C3%A1!%20Conheci%20a%20Galeria%20Sandra%20Novas%20pelo%20site%20e%20gostaria%20de%20agendar%20uma%20visita.%20Poderiam%20me%20passar%20os%20hor%C3%A1rios%20dispon%C3%ADveis%3F"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="block w-full h-full transition-all hover:scale-110"
